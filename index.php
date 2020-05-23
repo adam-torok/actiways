b85b6ea9f212f91524b38607f1bf754de5c41843
adam-torok <adam-torok@outlook.hu>
2020-05-23 19:10:05 +0200

    initials

---
+++ index.php
@@ -0,0 +1,10 @@
+
+    <?php include_once("public/components/header.html") ?>
+    <?php include_once("public/components/navbar.html") ?>
+
+<body>
+    <?php include_once("public/components/video-bg.html") ?>
+    <?php include_once("public/components/desc.html") ?>
+    <?php include_once("public/components/footer.html") ?>
+</body>
+</html>
